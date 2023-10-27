7d72904c1ed516ae90a8d905ec68d432f06f80b7
pauljuniordev <139752925+pauljunior0604@users.noreply.github.com>
2023-10-27 21:20:20 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Ex_Glasses from './Ex_Glasses/Ex_Glasses';
+
+function App() {
+  return (
+    <div className="container">
+      <Ex_Glasses/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Ex_Glasses/Ex_Glasses.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Ex_Glasses extends Component {
+  render() {
+    return (
+    <div>
+        <img src="./glassesImage/v1.png" alt="" />
+        <img src="./glassesImage/v2.png" alt="" />
+        <img src="./glassesImage/v3.png" alt="" />
+        <img src="./glassesImage/v4.png" alt="" />
+        <img src="./glassesImage/v5.png" alt="" />
+        <img src="./glassesImage/v6.png" alt="" />
+        <img src="./glassesImage/v7.png" alt="" />
+        <img src="./glassesImage/v8.png" alt="" />
+        <img src="./glassesImage/v9.png" alt="" />
+    </div>
+    )
+  }
+}
